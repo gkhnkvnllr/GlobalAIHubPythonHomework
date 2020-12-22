9bbb823f1026aa9b3acb31a563b0d8930145c4d5
gkhnkvnllr <gokhan.kovanlilar@bigo.digital>
2020-12-22 13:31:25 +0300

    Homework 1

---
+++ hw1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 13:22:08 2020
+
+@author: gkhnk
+"""
+
+# we can use for/while loop instead of taking 5 different variables manually
+
+v1 = int(input("Enter an integer for Value 1: "))
+print(f'Value 1 is {v1}')
+print("Type of Value 1 is",type(v1))
+
+v2 = int(input("Enter an integer for Value 2: "))
+print(f'Value 2 is {v2}')
+print("Type of Value 2 is",type(v2))
+
+v3 = input("Enter Value 3: ")
+print(f'Value 3 is {v3}')
+print("Type of Value 3 is",type(v3))
+
+v4 = input("Enter Value 4: ")
+print("Value 4 is {}".format(v4))
+print("Type of Value 4 is",type(v4))
+
+v5 = input("Enter Value 5: ")
+print("Value 5 is {}".format(v5))
+print("Type of Value 5 is",type(v5))
+
+# Gökhan Kovanlılar
